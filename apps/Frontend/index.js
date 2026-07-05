b6cdbd1547c0c7761b351299eaac98e215a226ea
Tristan <tristanhgriffiths8@yahoo.com>
2026-07-05 18:38:48 +0100

    sorting code

--- apps/Frontend/index.js
+++ apps/Frontend/index.js
@@ -4,22 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  const today = new Date();
-  let day = today.getDay();
-
-  //   console.log(day);
-  let type = "a weekday";
-  let adv = "It's time to work hard";
-
-  if (day === 0 || day === 6) {
-    type = "the weekend";
-    adv = "It's time to chill out";
-  }
-
-  res.render("index.ejs", {
-    dayType: type,
-    advice: adv,
-  });
+  res.render("index.ejs");
 });
 
 app.listen(port, () => {
