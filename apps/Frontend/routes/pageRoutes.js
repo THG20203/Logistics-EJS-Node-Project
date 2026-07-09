a2d75a61a20efc5944878c13e40da7e62192e6b0
Tristan <tristanhgriffiths8@yahoo.com>
2026-07-09 15:21:57 +0100

    more comment tidy up

--- apps/Frontend/routes/pageRoutes.js
+++ apps/Frontend/routes/pageRoutes.js
@@ -1,16 +1,17 @@
-/* Need router function from express, not whole object */
+/* Router function from express, not whole express object */
 import { Router } from "express";
 
-/* this file decides which controller function belongs to which URL. So needs 
-to know about your controller functions -> hence why homePage is imported from pageController.js. */
-/* route file needs to connect URL's to controller functions. */
+/* Routes file connects URL's to controller functions. */
 import { homePage } from "../controllers/pageController.js";
 
-/* Router(); creates a new router object */
+/* Create new router object */
 const router = Router();
 
-/* Router = collection of routes. Route = one method, one URL rule. Which URL was requested by client, 
-what function should handle it. i.e. GET method should run homePage controller */
+/* 
+Router = collection of routes. 
+Route = one HTTP method + URL path + controller function. 
+
+i.e. GET method should run homePage controller */
 
 router.get("/", homePage);
 // router.get("/about", aboutPage);
