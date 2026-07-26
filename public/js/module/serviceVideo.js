923eb287796d0f92d0279a47d420c67991bb5abc
Tristan <tristanhgriffiths8@yahoo.com>
2026-07-26 20:23:00 +0100

    sorting css and javascript for video final

--- public/js/module/serviceVideo.js
+++ public/js/module/serviceVideo.js
@@ -4,6 +4,12 @@ const play = wrapper.querySelector("button");
 
 /* when click button, add the playing class to the entire video-wrapper element */
 play.addEventListener("click", () => {
+  /* playing declaration forces opacity of gradient and the button to 0 */
   wrapper.classList.add("playing");
   video.play();
+});
+
+video.addEventListener("ended", () => {
+  wrapper.classList.remove("playing");
+  video.currentTime = 0;
 });
