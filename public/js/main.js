938cc1e3551c690680c1fe16737e8fa9b7e376d2
Tristan <tristanhgriffiths8@yahoo.com>
2026-08-04 21:00:33 +0100

    sorting files

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 // Import the site JavaScript
 import { initMobileMenu } from "./module/mobileMenu.js";
 import "./module/serviceVideo.js";
+import "./module/animation.js";
 
 // Run site javascript
 initMobileMenu();
